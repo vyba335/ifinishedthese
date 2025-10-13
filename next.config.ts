5785aeae4087917cfeb9aa8cc9e6c5fdff999bed
vyba335 <vyba335@gmail.com>
2025-10-13 17:05:51 +0200

    readme update

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,27 @@ const nextConfig: NextConfig = {
             },
         ],
     },
+    async headers() {
+        return [
+            {
+                source: "/:path*",
+                headers: [
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: "*",
+                    },
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET, POST, PUT, DELETE, OPTIONS",
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "X-Requested-With, Content-Type, Authorization",
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
